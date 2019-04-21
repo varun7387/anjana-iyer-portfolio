dc889ea7cab52f98208c67d2488f5266e2976ebc
Varun Ghaswala <varun7387@gmail.com>
2019-04-21 17:48:45 +1200

    reset gatsby with typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
+require(`dotenv`).config({ path: `.env` });
+
 module.exports = {
   siteMetadata: {
     name: `Hello Typescript World!`,
     tagline: `Gatsby + SASS + Typescript = 💪`
-  },  
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
-  ],
-}
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN
+      }
+    }
+  ]
+};
