6b029f146bda396f20e03b9f7b5ba1074faad03f
Varun Ghaswala <varun7387@gmail.com>
2019-04-23 08:03:06 +1200

    pulling in data from contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
-import { graphql } from 'gatsby';
-import * as React from 'react';
-import * as styles from './Index.module.scss';
+import { graphql } from "gatsby";
+import * as React from "react";
+import * as styles from "./Index.module.scss";
+import {
+  Document as ContentfulRichText,
+  BLOCKS,
+  MARKS
+} from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 interface IndexPageProps {
   data: {
@@ -8,8 +14,29 @@ interface IndexPageProps {
       siteMetadata: {
         name: string;
         tagline: string;
-      },
-    },
+      };
+    };
+    allContentfulProject: {
+      edges: [
+        {
+          node: {
+            id: string;
+            slug: string;
+            date: string;
+            title: {
+              title: string;
+            };
+            description: ContentfulRichText;
+            thumbnail: {
+              fluid: {
+                sizes: string;
+                src: string;
+              };
+            };
+          };
+        }
+      ];
+    };
   };
 }
 
@@ -21,21 +48,61 @@ export const indexPageQuery = graphql`
         tagline
       }
     }
+    allContentfulProject(sort: { fields: [date] }) {
+      edges {
+        node {
+          id
+          slug
+          date(formatString: "MMM YYYY")
+          title {
+            title
+          }
+          description {
+            json
+          }
+          thumbnail {
+            fluid(maxWidth: 512) {
+              sizes
+              src
+            }
+          }
+        }
+      }
+    }
   }
 `;
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
+  public renderProjectTile = (project: any, index: number) => {
+    return (
+      <div key={index}>
+        <img src={project.thumbnail.fluid.src} />
+        <h2>{project.title.title}</h2>
+        <strong>{project.date}</strong>
+        <br />
+        <strong>{project.slug}</strong>
+        <br />
+        <p>{project.description.json.value}</p>
+      </div>
+    );
+  };
 
   public render() {
-    const {
-      name,
-      tagline,
-    } = this.props.data.site.siteMetadata;
+    const { name, tagline } = this.props.data.site.siteMetadata;
+    const projects = this.props.data.allContentfulProject.edges.map(
+      edge => edge.node
+    );
 
     return (
       <div className={styles.Container}>
         <h1>{name}</h1>
         <p>{tagline}</p>
+
+        <div className={styles.Projects}>
+          {projects.map((project, index) =>
+            this.renderProjectTile(project, index)
+          )}
+        </div>
       </div>
     );
   }
